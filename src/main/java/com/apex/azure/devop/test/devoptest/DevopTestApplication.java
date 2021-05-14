776b3147ac65753d2f004d03d4a02f0dd75c2391
xiuxian.song <xiuxian.song@apexglobe.com>
2021-05-13 21:05:02 -0700

    init

---
+++ src/main/java/com/apex/azure/devop/test/devoptest/DevopTestApplication.java
@@ -0,0 +1,13 @@
+package com.apex.azure.devop.test.devoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/azure/devop/test/devoptest/DevopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.azure.devop.test.devoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
